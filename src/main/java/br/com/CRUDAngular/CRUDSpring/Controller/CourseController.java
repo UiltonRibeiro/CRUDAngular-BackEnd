49e93bd0309ba1f4bce1627fd91dba3eda19062f
Uilton <uilton_3001@outlook.com>
2023-09-28 16:47:46 -0300

    Feat:[Add] Criando método Post de Course

--- src/main/java/br/com/CRUDAngular/CRUDSpring/Controller/CourseController.java
+++ src/main/java/br/com/CRUDAngular/CRUDSpring/Controller/CourseController.java
@@ -3,9 +3,9 @@ package br.com.CRUDAngular.CRUDSpring.Controller;
 import br.com.CRUDAngular.CRUDSpring.Model.Course;
 import br.com.CRUDAngular.CRUDSpring.Repository.CourseRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +19,12 @@ public class CourseController {
     @GetMapping
     public List<Course> list(){
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course createCourse(@RequestBody Course course){
+        return courseRepository.save(course);
     }
 
 }
